7e096ae583908c7e97c5e3a994cd809b86439726
Leonardo Dantas <leonardosdantas@protonmail.com>
2016-04-09 17:07:36 -0300

    Programa para calcular frequencia de letras.

---
+++ frequencia.py
@@ -0,0 +1,13 @@
+#Programa: frequencia.py
+#Descrição: Calcula frequencia de letras em um texto.
+
+def calcular_frequencia(texto):
+    alfabeto = { 'a': 0, 'b': 0, 'c': 0, 'd': 0, 'e': 0, 'f': 0, 'g': 0, 'h': 0, 'i': 0, 'j': 0, 'k': 0, 'l': 0, 'm': 0, 'n': 0, 'p': 0, 'p': 0, 'q': 0, 'r': 0, 's': 0, 't': 0, 'u': 0, 'v': 0, 'x': 0, 'y': 0, 'z': 0 }
+
+    for letra in texto.lower():
+        if letra in alfabeto:
+            alfabeto[letra] += 1
+
+    frequencia = sorted(alfabeto.items())
+
+    return frequencia
